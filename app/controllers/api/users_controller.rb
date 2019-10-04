0cf01732525dc24c5564b7682cbf8bbf7ca8516e
Scott-Park <scott@scottjpark.com>
2019-10-04 16:15:23 -0400

    Add more signup error messages

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,14 +2,16 @@ class Api::UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      login(@user)
-      render 'api/users/show'
-    else
-      if user_params[:password].length < 6
+    if user_params[:password].length < 6
         render json: ['Passwords need to be 6 or more characters'], status: 411
-      elsif User.find_by(username: user_params[:username])
-        render json: ['Username already taken'], status: 409
+    elsif User.find_by(username: user_params[:username])
+      render json: ['Username already taken'], status: 409
+    elsif !user_params[:email].split("").include?('@')
+      render json: ['Please use a valid e-mail address'], status: 406
+    else
+      if @user.save
+        login(@user)
+        render 'api/users/show'
       else
         render json: ['Something went wrong'], status: 406
       end
@@ -28,7 +30,7 @@ class Api::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :email)
   end
 
 end
